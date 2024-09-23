fef04555b9c086284f433d65c1a92f391d3ab22f
Nivedhan <chagananivedhan@gmail.com>
2024-09-23 16:34:55 +0530

    Daily task

---
+++ Daily_task.js
@@ -0,0 +1,38 @@
+
+
+    // DATA TYPES INTRODUCTION 
+    let name1="Nivedhan";
+    let Age=21;
+    let Job=null
+    
+         console.log("This is",name1,"Thye type is",typeof(name1))
+         console.log("This is",Age,"Thye type is",typeof(Age))
+       console.log(10>5, "This is Boolean")
+       console.log("this is",typeof x)
+       console.log("this is",typeof Job)
+       
+       //   Mathematical Operators
+       
+       let num1=200;
+       let num2=300;
+       console.log("The Addditon of 2 numbers is:",num1+num2);
+       console.log("The Substraction of 2 numbers is:",num1-num2);
+       console.log("The Division of 2 numbers is:",num1/num2);
+       console.log("The Multiplication of 2 numbers is:",num1*num2);
+       console.log("The Modulus of 2 numbers is:",num1%num2);
+       console.log("The Squaring of 2 numbers is:",num1**num2);
+       
+       //  COMPARATOR OPERATORS
+       let a=100
+       let b=120
+       console.log(`This is == operator of ${a} and ${b}`,a==b)
+       console.log(`This is === operator of ${a} and ${b}`,a===b)
+       console.log(`This is != operator of ${a} and ${b}`,a!=b)
+       console.log(`This is !== operator of ${a} and ${b}`,a!==b)
+       console.log(`This is > operator of ${a} and ${b}`,a>b)
+       console.log(`This is < operator of ${a} and ${b}`,a<b)
+       console.log(`This is <= operator of ${a} and ${b}`,a<=b)
+       console.log(`This is >= operator of ${a} and ${b}`,a>=b)
+       
+
+
